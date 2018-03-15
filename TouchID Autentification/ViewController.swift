1255398c59699062c0be353aa48df58a2ba409f9
Jorge <wiijlg@hotmail.com>
2018-03-15 16:56:04 +0100

    Interfaz Visual

--- TouchID Autentification/ViewController.swift
+++ TouchID Autentification/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // # Variables Especificas
+    @IBOutlet weak var labelPrincipal: UILabel!
+    @IBOutlet weak var textUsuario: UITextField!
+    @IBOutlet weak var textPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // # Metodos Botones
+    @IBAction func btnAcceder(_ sender: UIButton) {
+        if (textUsuario.text?.elementsEqual("JORGE"))! {
+            if (textPassword.text?.elementsEqual("0000"))!{
+                labelPrincipal.textColor = UIColor.green
+                labelPrincipal.text = "Hola JORGE"
+            } else {
+                labelPrincipal.textColor = UIColor.red
+                labelPrincipal.text = "Contraseña incorrecta!"
+            }
+        } else {
+            labelPrincipal.textColor = UIColor.black
+            labelPrincipal.text = "Usuario " + textUsuario.text! + " no existe"
+        }
+    }
+    
+    @IBAction func btnAccederTouchID(_ sender: Any) {
+        
+    }
+    
 }
 
