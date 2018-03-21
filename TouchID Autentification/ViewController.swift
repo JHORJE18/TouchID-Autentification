b0e513bf0c1ab4fd8b36525dde0e034734967d14
Jorge <wiijlg@hotmail.com>
2018-03-21 12:32:48 +0100

    Recibe respuestas acciones rápidas de las notificaciones

--- TouchID Autentification/ViewController.swift
+++ TouchID Autentification/ViewController.swift
@@ -43,6 +43,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
             }
         }
         
+        // Declara este archivo para recibir las reacciones de las notificaciones
         UNUserNotificationCenter.current().delegate = self
     }
 
@@ -67,19 +68,29 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         }
     }
     
+    // Control de botones de Notificación
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // TODO Gestionar posiblidades o opciones
+        switch response.actionIdentifier {
+        case "rememberAction":
+            print("Vale, le avisare más tarde")
+            solicitarNotificacion()
+            break
+        case "deleteAction":
+            print("Vale, elimino la notificación")
+            UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: ["cocoacasts_local_notification"])
+            break
+        default:
+            print("No se de donde ha venido")
+            break
+        }
+    }
+    
     @IBAction func selectorChanged(_ sender: UISlider) {
         labelTiempo.text = String( Int(selector.value * 100))
     }
     
-    // Enviar nueca notificación
-    @IBAction func btnNuevaNotificacion(_ sender: UIButton) {
-        let alertaNotificacion = UIAlertController(title: "Notificación solicitada", message: "Se ha solicitado una notificación para dentro de " + String(Int(selector.value * 100)) + " segundos", preferredStyle: .alert)
-        alertaNotificacion.addAction(UIAlertAction(title: "Aceptar", style: .default))
-        alertaNotificacion.addAction(UIAlertAction(title: "Cancelar", style: .destructive))
-        
-        self.present(alertaNotificacion, animated: true)
-        
-        // Creamos notificación
+    func solicitarNotificacion(){
         let notificacionBurbuja = UNMutableNotificationContent()
         notificacionBurbuja.title = "Se ha acabado!"
         notificacionBurbuja.body = "Ya era hora de terminar de esta pesadilla"
@@ -95,7 +106,27 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
             }
         }
         
+        // Añadimos acciones de la notificación
+        
+        //1. Creamos la acción de Recordar
+        let rememberAction = UNNotificationAction(identifier: "rememberAction", title: "Recordar más tarde", options: [])
+        
+        //2. Creamos la acción de Eliminar
+        let deleteAction = UNNotificationAction(identifier: "deleteAction", title: "Eliminar Notificación", options: [])
+        
+        //3. Creamos la categoría que agrupa las acciones
+        let category = UNNotificationCategory(identifier: "cocoacasts_local_notification_category", actions: [rememberAction, deleteAction], intentIdentifiers: [], options: [])
+        
+        // Asigna la cateogira de la notificación actual
+        notificacionBurbuja.categoryIdentifier = "cocoacasts_local_notification_category"
+        
+        //4. Añadimos la categoría al Centro de Notificaciones
+        UNUserNotificationCenter.current().setNotificationCategories([category])
+        
+        // Crear disparador de la notificación
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(Int(selector.value * 100)), repeats: false)
+        
+        // Creamos la Request con su identificador, contenido de la notificación y el disparador de la notificacion
         let notificationRequest = UNNotificationRequest(identifier: "cocoacasts_local_notification", content: notificacionBurbuja, trigger: trigger)
         
         print("Cargando notificacion para dentro de " , (trigger.timeInterval))
@@ -109,13 +140,26 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         }
     }
     
+    // Enviar nueca notificación
+    @IBAction func btnNuevaNotificacion(_ sender: UIButton) {
+        let alertaNotificacion = UIAlertController(title: "Notificación solicitada", message: "Se ha solicitado una notificación para dentro de " + String(Int(selector.value * 100)) + " segundos", preferredStyle: .alert)
+        alertaNotificacion.addAction(UIAlertAction(title: "Aceptar", style: .default))
+        alertaNotificacion.addAction(UIAlertAction(title: "Cancelar", style: .destructive))
+        
+        self.present(alertaNotificacion, animated: true)
+        
+        // Creamos notificación
+        solicitarNotificacion()
+    }
+    
+    // Boton acceder mediante TouchID
     @IBAction func btnAccederTouchID(_ sender: Any) {
-        if (!textUsuario.isEqual("")){
-            self.autentificarUsuario()
-        } else {
+        if (textUsuario.text?.isEqual(""))!{
             let alertUser = UIAlertController(title: "Error!", message: "No has introducido ningun Usuario con el que Iniciar Sesión.", preferredStyle: .alert)
             alertUser.addAction(UIAlertAction(title: "OK", style: .default))
             self.present(alertUser, animated: true)
+        } else {
+            self.autentificarUsuario()
         }
     }
     
@@ -149,7 +193,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
             }
         } else {
             // Dispositivo no compatible o no preparado para usar TouchID
-            //  En el caso de iPhone X usara FaceID en vez de TouchID
+            // En el caso de iPhone X usara FaceID en vez de TouchID
             let ac = UIAlertController(title: "Touch ID no dispondible", message: "Tu dispositivo no soporta TouchID.", preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "OK", style: .default))
             present(ac, animated: true)
@@ -161,6 +205,8 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         
         completionHandler([.alert])
     }
+    
+    
 
     
 }
